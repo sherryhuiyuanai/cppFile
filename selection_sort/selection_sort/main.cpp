843b20028ebee22d09f6c7add91655c2f474ed27
sherryhuiyuanai <sherryhuiyuanai@gmail.com>
2019-05-29 22:35:58 -0400

    created the selection sort

---
+++ selection_sort/selection_sort/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  selection_sort
+//
+//  Created by Sherry R.Z on 2019-05-29.
+//  Copyright © 2019 Sherry R.Z. All rights reserved.
+//
+
+/*
+ 
+ Selection Sort:
+ the simplest method for sorting,
+ but also the most useless method, since the time complexity is O(n^2)
+ also it's not stable!
+ 
+ It's rarely used in engineering area.
+ 
+ */
+
+#include <iostream>
+
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
