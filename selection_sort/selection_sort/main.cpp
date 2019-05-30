49b93cdeb78f71284310be1cfa55984e113e961a
sherryhuiyuanai <sherryhuiyuanai@gmail.com>
2019-05-30 11:36:45 -0400

    in cpp, length = sizeof(array)/sizeof(array[0])

--- selection_sort/selection_sort/main.cpp
+++ selection_sort/selection_sort/main.cpp
@@ -37,6 +37,6 @@ int main(int argc, const char * argv[]) {
 //    for(int i = 0; i < sizeof(arr); i++){
 //        cout << arr[i];
 //    }
-    cout << sizeof(arr) << endl;
+    cout << sizeof(arr)/sizeof(arr[0]) << endl;
     return 0;
 }
